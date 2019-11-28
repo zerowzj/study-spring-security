f3a999bd38065a04b83c23f9a3f592644c45eee4
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-28 18:09:30 +0800

    更新代码

--- src/main/java/study/spring/security/auth/user/CustomUserDetails.java
+++ src/main/java/study/spring/security/auth/user/CustomUserDetails.java
@@ -1,6 +1,5 @@
 package study.spring.security.auth.user;
 
-import study.spring.security.common.web.session.SessionUserInfo;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -13,28 +12,24 @@ import java.util.List;
  */
 public class CustomUserDetails implements UserDetails {
 
-    /** 登录名 */
+    /**
+     * 登录名
+     */
     private String loginName = null;
-    /** 登录密码 */
+    /**
+     * 登录密码
+     */
     private String loginPwd = null;
-    /** 授权 */
+    /**
+     * 授权
+     */
     private List<SimpleGrantedAuthority> grantedAuthLt = null;
 
-    /** 用户信息 */
-    private SessionUserInfo userInfo = null;
 
     public CustomUserDetails(String loginName, String loginPwd, List<SimpleGrantedAuthority> grantedAuthLt) {
         this.loginName = loginName;
         this.loginPwd = loginPwd;
         this.grantedAuthLt = grantedAuthLt;
-    }
-
-    public SessionUserInfo getUserInfo() {
-        return userInfo;
-    }
-
-    public void setUserInfo(SessionUserInfo userInfo) {
-        this.userInfo = userInfo;
     }
 
     @Override
